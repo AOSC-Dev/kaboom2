6551a77ff76233e5a67b82d5a734610277a6e2e6
Xinhui Yang <cyan@cyano.uk>
2025-11-25 13:56:09 +0800

    stage0: unset continuation flag only if the stage matches

--- proc/40-stage0.bash
+++ proc/40-stage0.bash
@@ -19,6 +19,6 @@ execute_sequence || {
 }
 
 # Clear the continuation flag
-if [ -n "$KABOOM_CONTINUE_STAGE" ] ; then
+if [ -n "$KABOOM_CONTINUE_STAGE" ] && [ "$KABOOM_CONTINUE_STAGE" == "$KABOOM_CUR_STAGE" ] ; then
 	unset KABOOM_CONTINUE_STAGE KABOOM_CONTINUE_PACKAGE
 fi
